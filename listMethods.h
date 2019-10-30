538a28437e70dfb2bb8bda2a8dcb105f5189eb5d
Sharon Zou <szou00@stuy.edu>
2019-10-30 10:39:32 -0400

    added two functions

--- listMethods.h
+++ listMethods.h
@@ -5,5 +5,5 @@ struct song_node{
 };
 
 struct song_node * insert_front(struct song_node *s, char *n, char *a);
-
-void * insert_inorder(struct song_node *s, char *n, char *a);
+void insert_inorder(struct song_node *s, char *n, char *a);
+struct song_node * find_song(struct song_node *s, char *n, char *a);
--- songs.c
+++ songs.c
@@ -1,34 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "listMethods.h"
 
-struct song_node * insert_front(struct song_node *n, int x){
-  struct song_node *newSong = (struct song_node*)malloc(sizeof(struct song_node));
-  newSong->i = x;
-  newSong->next = n;
+//insert nodes at the front
+struct song_node * insert_front(struct song_node *s, char *n, char *a){
+  struct song_node* newSong = (struct song_node *)malloc(sizeof(s));
+  newSong->name = n;
+  newSong->artist = a;
+  newSong->next = s;
   return newSong;
 }
 
-void * insert_inorder(struct song_node *s, char *n, char *a){
-  struct song_node *newSong = (struct song_node*)malloc(sizeof(struct song_node));
+//insert nodes in order - alphabetical by Artist then by Song
+void insert_inorder(struct song_node *s, char *n, char *a){
+  /**
+  struct song_node* newSong = (struct song_node *)malloc(sizeof(n));
   newSong->name = n;
   newSong->artist = a;
-  while ((s->next) != NULL && (s->a).compareTo(a) < 0){
-    s = s.next;
+  while ((s->next) != NULL && (s->artist).compareTo(a) < 0){
+    s = s->next;
   }
-  while ((s->next) != NULL && (s->n).compareTo(n) < 0 && (s->a).compareTo(a) == 0){
-    s = s.next;
+  while ((s->next) != NULL && (s->name).compareTo(n) < 0 && (s->artist).compareTo(a) == 0){
+    s = s->next;
   }
   if (s->next == NULL){
     s->next = newSong;
-    newSong->next = NULL
+    newSong->next = NULL;
   }
   else{
-    struct song_node *temp = (struct song_node*)malloc(sizeof(struct song_node));
+    struct song_node* temp = (struct song_node *)malloc(sizeof(n));
     temp = s->next;
     s->next = newSong;
     newSong->next = temp;
     free(temp);
     temp = NULL;
   }
+  **/
+}
+//sorry i commented this out first bc it was giving errors but i wasn't sure how to fix them 
+
+//print the entire list
+void print_list(struct song_node *n) {
+  while (n != NULL) {
+    printf(" %s : %s |", n->artist, n->name);
+    n = n->next;
+  }
+  printf("%s\n", "]");
+}
+
+//find and return a pointer to a node based on artist and song name
+struct song_node * find_song(struct song_node *s, char *n, char *a) {
+  while (s!=NULL) {
+    if (s->name == n && s->artist == a) {
+      return s;
+    }
+  }
+  return NULL; //if it's not found - not sure if this is what he wants though
 }
